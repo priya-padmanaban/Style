a6e26aa1ed8d64ecbbae0f732db64b7fdb29c658
mkouzmin <mkouzmin@ucsc.edu>
2018-07-19 17:39:35 -0700

    Update learn_class.py

--- learn_class.py
+++ learn_class.py
@@ -1,33 +1,36 @@
-import pandas as pd, numpy, pickle
-from sklearn import model_selection, preprocessing, metrics, linear_model, svm, ensemble
+import pandas as pd
+from sklearn import preprocessing, ensemble
 from sklearn.externals import joblib
 
-input_file = "authors.csv"
+input_file = "samples.csv"
 df = pd.read_csv(input_file, header = 0)
-test_file = "testing.csv"
+test_file = "samples1.csv"
 df1 = pd.read_csv(test_file, header = 0)
 
 
 encoder = preprocessing.LabelEncoder()
-train_lab = encoder.fit_transform(df['author'])
+train_lab = df['author']
 train_data = df.iloc[:,1:]
 test_data = df1.iloc[:,1:]
-test_lab = encoder.fit_transform(df1['author'])
-def train(classifier, train_features, train_label, test_features):
-    model = classifier.fit(train_features, train_label)# model
+test_lab = df1['author']
+RFC = ensemble.RandomForestClassifier(n_estimators = 1000)
+model = RFC.fit(train_data, train_lab)# model
 
-    preds = model.predict(test_features)#preds = predicted values
-    print(preds)
-    return metrics.accuracy_score(preds, test_lab)
+preds = model.predict(test_data)#preds = predicted values
+print(preds)
 
-accuracy = train(ensemble.RandomForestClassifier(n_estimators = 1000), train_data, train_lab , test_data)
-print ("Random_Forest:", accuracy)
 
 
 #get model, then implement:
-#import pickle
-#s = joblib.dump(model, "file.pkl")
+s = joblib.dump(model, "file.pkl")
 
 
-#model2 = joblib.load("file.pkl")
-#model2.predict(test_data)
+
+#put in 2nd file of code
+
+import pandas as pd
+from sklearn import preprocessing, ensemble
+from sklearn.externals import joblib
+model2 = joblib.load("file.pkl")
+preds2 = model2.predict(test_data)
+print(preds2[0])
